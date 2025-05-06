0c1bb6a158ed34292e9feeae09b4bfb26953ec62
suzan <suzan@gmail.com>
2025-05-07 01:03:34 +0300

    "Sign_Up Pages&Backend"

---
+++ lib/core/constants/Colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFA5C8A6);      // الأخضر الفاتح
+  static const Color secondary = Color(0xFFE56B50);   // البرتقالي
+  static const Color background = Color(0xFFE8DCCF);  // بيج فاتح
+  static const Color onPrimary = Color(0xFF3D3D3D);   // للنص على اللون الأساسي
+}
