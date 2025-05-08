3d05b3e6bfa1b33f85b11702d47c9fad9ccbecf2
Dana Morgan <dana.morgan196@gmail.com>
2025-05-09 01:12:09 +0300

    add theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:matbakhna_mobile/features/recipes/screens/recipe_detail_screen.dart';
+import 'core/constants/theme.dart';
 import 'features/home/screens/home_screen.dart';
 
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -13,10 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Smart Recipe App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      theme: appTheme,
       home: const HomePage(),
     );
   }
