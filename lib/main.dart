8d8b0a65b4c96a587efba2c3c154c5177f439391
suzan <suzan@gmail.com>
2025-05-17 17:26:54 +0300

    Save local changes before merging main into suzan

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
-
 import 'package:flutter/material.dart';
-import 'features/recipes/screens/recipe_detail_screen.dart';
 
-void main() async{
+import 'features/auth/screens/signup_screen.dart';
+import 'features/home/screens/home_screen.dart';
+
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
   runApp(const MyApp());
@@ -16,17 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Smart Recipe App',
-      theme: ThemeData(
-        colorScheme: const ColorScheme.light(
-          primary: Color(0xFFA5C8A6),      // الأخضر الفاتح
-          secondary: Color(0xFFE56B50),   // البرتقالي
-          background: Color(0xFFE8DCCF),  // بيج فاتح
-          onPrimary: Color(0xFF3D3D3D),   // للنص على اللون الأساسي
-        ),
-        useMaterial3: true,
-      ),
-
-      home: const RecipePage(),
+      home: const SignUpStepOnePage(),
     );
   }
 }
