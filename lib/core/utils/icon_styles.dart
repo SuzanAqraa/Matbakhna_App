cb124eef5f1d7b144d2b9a8e3c9c3faf6dd69bbe
suzan <suzan@gmail.com>
2025-05-17 20:13:24 +0300

    "add themes "

---
+++ lib/core/utils/icon_styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class IconStyle {
+  // Bottom Navigation Bar Icon Colors
+  static const Color selectedItemColor = Color(0xFFE56B50);
+  static const Color unselectedItemColor = Colors.grey;
+
+  // Default Icon Style
+  static const Color defaultIconColor = Colors.black;
+  static const double defaultIconSize = 24.0;
+}
---
+++ lib/core/utils/textfeild_styles.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class ThemeTextStyle {
+  static TextStyle titleTextFieldStyle = const TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+    color: Color(0xFF3D3D3D),
+  );
+  static TextStyle recipeNameTextFieldStyle = const TextStyle(
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    color: Color(0xFF3D3D3D),
+  );
+  static TextStyle bodySmallTextFieldStyle = const TextStyle(
+    fontSize: 13,
+    color: Color(0xFF707070),
+  );
+  static TextStyle interActionTextFieldStyle = const TextStyle(
+    color: Colors.black,
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
+  );
+  static TextStyle ButtonTextFieldStyle = const TextStyle(
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+
+  );
+}
