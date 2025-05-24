63f1912ffc5b2aa80d43df98ee5e8f10ae58726b
mennajihad12 <mennajihad12@gmail.com>
2025-05-24 09:18:19 +0000

    listing screen and favorate screen

--- lib/features/listing/screens/listing_screen.dart
+++ lib/features/listing/screens/listing_screen.dart
@@ -1,28 +1,68 @@
 import 'package:flutter/material.dart';
-
-import '../../../core/widgets/PrimaryAppBar.dart';
-import '../../../core/widgets/custom_bottom_navbar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:matbakhna_mobile/core/widgets/PrimaryAppBar.dart';
+import 'package:matbakhna_mobile/core/widgets/cards.dart';
+import 'package:matbakhna_mobile/core/widgets/custom_bottom_navbar.dart';
 
 class ListingScreen extends StatelessWidget {
-  const ListingScreen({Key? key}) : super(key: key);
+  const ListingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: const CustomBottomNavbar(currentIndex: 3),
       backgroundColor: const Color(0xFFFDF5EC),
-      body: Directionality(
-        textDirection: TextDirection.rtl,
-        child: SingleChildScrollView(
+
+      body: SafeArea(
+        child: Directionality(
+          textDirection: TextDirection.rtl,
           child: Column(
-            children: const [
-              HomeAppBar(title: 'صفحة التصفح'),
-              SizedBox(height: 20),
+            children: [
+              const HomeAppBar(title: 'صفحة التصفح'),
+
+              Expanded(
+                child: StreamBuilder(
+                  stream: FirebaseFirestore.instance.collection('recipes').snapshots(),
+                  builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(child: CircularProgressIndicator());
+                    }
+                    if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                      return const Center(child: Text('لا توجد وصفات متاحة.'));
+                    }
+
+                    return ListView.builder(
+                      itemCount: snapshot.data!.docs.length,
+                      itemBuilder: (context, index) {
+                        var data = snapshot.data!.docs[index].data() as Map<String, dynamic>;
+
+                        String imageUrl = data['imageUrl']?.toString() ?? '';
+                        String title = data['Title']?.toString() ?? 'بدون اسم';
+                        String description = data['Description']?.toString() ?? 'لا يوجد وصف';
+                        String time = data['duration']?.toString() ?? 'غير محدد';
+                        int numLikes = data['Num_Likes'] ?? 0;
+                        int numComments = data['Num_Comments'] ?? 0;
+
+                        return SizedBox(
+                          height: 300,
+                          child: CardListing(
+                            imageUrl: imageUrl,
+                            title: title,
+                            description: description,
+                            time: time,
+                            numLikes: numLikes,
+                            numComments: numComments,
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
       ),
-      bottomNavigationBar: CustomBottomNavbar(currentIndex: 3),
-
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:matbakhna_mobile/features/favorites/screens/favorites_screen.dart';
 import 'features/auth/providers/auth_service.dart';
 import 'features/home/screens/home_screen.dart';
+import 'features/listing/screens/listing_screen.dart';
 
 
 Future<void> main() async {
@@ -25,7 +27,7 @@ class MyApp extends StatelessWidget {
           child: child!,
         );
       },
-      home: const HomePage(),
+      home:  FavoritesScreen(),
     );
   }
 }
